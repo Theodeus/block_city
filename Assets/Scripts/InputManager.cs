4a1a360296919b82b20d8303160cd018a13a7b05
Oskar Eriksson <oskar.det@gmail.com>
2018-03-23 08:38:35 +0100

    Placing blocks and moving citizens changing colors, rotating camera

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public Camera cam;
+    public List<Transform> prefabBuildings;
+
+    private System.Random rand;
+
+    void Start()
+    {
+        cam = Camera.main;
+        rand = new System.Random();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            int layerMask = 1 << 8;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                Debug.Log(hit.point);
+                //Debug.DrawLine(ray.origin, hit.point);
+                if (Mathf.Floor(hit.point.y) == 0)
+                {
+                    Instantiate(prefabBuildings[rand.Next(0, prefabBuildings.Count)], new Vector3(Mathf.Floor(hit.point.x), Mathf.Round(hit.point.y), Mathf.Floor(hit.point.z) + 1), Quaternion.identity);
+                }
+                else
+                {
+                    
+                    Instantiate(prefabBuildings[rand.Next(0, prefabBuildings.Count)], new Vector3(hit.transform.position.x, Mathf.Round(hit.point.y), hit.transform.position.z), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+    private NavMeshAgent agent;
+
+    public float acceleration = 2f;
+    public float deceleration = 60f;
+    public float closeEnoughMeters = 4f;
+
+    private Dictionary<string, Color> buildings = new Dictionary<string, Color>();
+
+    void Start()
+    {
+        agent = gameObject.GetComponentInChildren<NavMeshAgent>();
+        buildings.Add("Water", new Color(.4f, .7f, 1));
+        buildings.Add("Food", new Color(1, .3f, .6f));
+        buildings.Add("Housing", new Color(.5f, 1, .5f));
+    }
+
+    void Update()
+    {
+        if (agent)
+        {
+            if (agent.hasPath)
+            {
+                // speed up slowly, but stop quickly
+                agent.acceleration = (agent.remainingDistance < closeEnoughMeters) ? deceleration : acceleration;
+            }
+            else
+            {
+                // set random destination
+                Vector3 destination = new Vector3(Random.Range(0, 10), 0, Random.Range(0, 10));
+                agent.SetDestination(destination);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MeshRenderer mesh = gameObject.GetComponentInChildren<MeshRenderer>();
+        Color newColor;
+        if (buildings.TryGetValue(other.gameObject.tag, out newColor))
+        {
+            mesh.material.color = newColor;
+        }
+    }
+}
